0f11093caa83666aaa9f5c3dfc9f9b2f677c20e6
NicoleMoix <nicole.moix@inacapmail.cl>
2026-03-09 09:55:49 -0300

    img

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './css/global.css'
 import './css/navbar.css'
-
+import './css/carousel.css'
+import './css/catalog.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
