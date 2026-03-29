04f99d33979bfe1680b802158e15bb765b682c41
NicoleMoix <nicole.moix@inacapmail.cl>
2026-03-28 23:17:29 -0300

    bdd base

---
+++ frontend/src/bdd-local.js
@@ -0,0 +1,61 @@
+export const catalogo =[
+        {amigurumi:{
+            categoria:"",
+            
+            img_amig_front:"",
+            img_amig_back:"",
+            nombre_amig:"",
+        },
+        patron:{
+            idioma:{
+                español:{
+                    url:"https://www.flaticon.es/iconos-gratis/espana",
+                    alt:"España icon (creado por Freepik - Flaticon)",
+                },
+                ingles:{
+                    url:"https://www.flaticon.es/iconos-gratis/estados-unidos",
+                    alt:"Estados Unidos icon (creado por Roundicons - Flaticon)",
+                },
+            },
+            contenido:[
+                "Patrón escrito paso a paso",
+                "Fotos de referencia",
+                "Consejos para un mejor acabado",
+            ],
+            valor:{
+                gratis:"Gratuito",
+                pago:"De Pago",
+            },
+            inacapito:{
+                img_portada:"/img/personajes/inacapito/principal.jpg",
+                nombre:"Inacapito",
+                creadora:"Nicole Moix - NikyArte",
+                available:"Disponible",
+                link_raverly:"https://ravel.me/inacapito",
+                dificultad:"Fácil",
+                descripcion:[
+                    "¡Teje a INACAPITO, la tierna y simpática mascota del Instituto Profesional INACAP! ❤️",
+                    "Este patrón está pensado para quienes buscan un proyecto pequeño, divertido y lleno de identidad chilena.",
+                    "Ideal para quienes aman los detalles y buscan una forma original de homenajear a su institución. Perfecto como llavero, adorno o recuerdo institucional. ¡Animate a darle vida a INACAPITO con tus propias manos! ✨"
+                ],
+                materiales: [
+                    "Hilo Ukryl (rojo)",
+                    "Hilo de bordar negro y rosa",
+                    "Pintura acrílica negra y blanca",
+                    "Silicona fría",
+                    "Tijera y aguja",
+                    "Plasticola y agua (para endurecer)"
+                ],
+                creacion:"2025",
+                gallery:{
+                    img_1:"/img/personajes/inacapito/principal.jpg",
+                    img_2:"/img/personajes/inacapito/secundaria.jpg",
+                    img_3:"/img/personajes/inacapito/terciaria.jpg",
+                },
+            },
+            
+
+        },
+        
+    }
+]
